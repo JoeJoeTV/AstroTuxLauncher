2fca1c4cb92d799e301c0b632723001440b8ec4e
Johannes Schöneberger <hannes1033@gmail.com>
2023-08-07 02:53:11 +0200

    Added asynchronous notification handler

--- utils/interface.py
+++ utils/interface.py
@@ -2,6 +2,7 @@ import threading
 import time
 import asyncio
 from enum import Enum
+from queue import Queue
 
 class KeyboardThread(threading.Thread):
     """
@@ -22,6 +23,17 @@ class KeyboardThread(threading.Thread):
             else:
                 time.sleep(0.1)
 
+
+class EventType(Enum):
+    MESSAGE = "message"
+    START = "start"
+    REGISTERED = "registered"
+    SHUTDOWN = "shutdown"
+    CRASH = "crash"
+    PLAYER_JOIN = "player_join"
+    PLAYER_LEAVE = "player_leave"
+    COMMAND = "command"
+
 class NotificationManager:
     """ Class that keeps multiple Notification Handlers and broadcasts messages to all of them """
     
@@ -40,17 +52,6 @@ class NotificationManager:
         for handler in self.handlers:
             handler.send_event(event_type, **params)
 
-
-class EventType(Enum):
-    MESSAGE = "message"
-    START = "start"
-    REGISTERED = "registered"
-    SHUTDOWN = "shutdown"
-    CRASH = "crash"
-    PLAYER_JOIN = "player_join"
-    PLAYER_LEAVE = "player_leave"
-    COMMAND = "command"
-
 def safeformat(str, **kwargs):
     """
         Formats the passed string {str} using the given keyword arguments, while keeping missing replacements unformatted
@@ -64,35 +65,90 @@ def safeformat(str, **kwargs):
     
     return str.format_map(replacements)
 
-class NotificationHandler():
+DEFAULT_EVENT_FORMATS = {
+        EventType.MESSAGE: "[{name}] {message}",
+        EventType.START: "[{name}] Server started!",
+        EventType.REGISTERED: "[{name}] Server registered with Playfab!",
+        EventType.SHUTDOWN: "[{name}] Server shutdown!",
+        EventType.CRASH: "[{name}] Server crashed!",
+        EventType.PLAYER_JOIN: "[{name}] Player '{player}' joined the game",
+        EventType.PLAYER_LEAVE: "[{name}] Player '{player}' left the game",
+        EventType.COMMAND: "[{name}] Command executed: {command}"
+    }
+
+class NotificationHandler:
     """
         A class that can receive events and send them along as formatted messages to some endpoint
     """
     
-    DEFAULT_FORMATS = {
-        EventType.MESSAGE: "[{server_name}] {message}",
-        EventType.START: "[{server_name}] Server started!",
-        EventType.REGISTERED: "[{server_name}] Server registered with Playfab!",
-        EventType.SHUTDOWN: "[{server_name}] Server shutdown!",
-        EventType.CRASH: "[{server_name}] Server crashed!",
-        EventType.PLAYER_JOIN: "[{server_name}] Player '{player}' joined the game",
-        EventType.PLAYER_LEAVE: "[{server_name}] Player '{player}' left the game",
-        EventType.COMMAND: "[{server_name}] Command executed: {command}"
-    }
-    
-    def __init__(self, server_name="Gameserver", event_whitelist=set([e for e in EventType]), event_formats=DEFAULT_FORMATS):
-        self.server_name = server_name
+    def __init__(self, name="Server", event_whitelist=set([e for e in EventType]), event_formats=DEFAULT_EVENT_FORMATS):
+        self.name = name
         self.whitelist = event_whitelist
         self.formats = event_formats
     
     def send_event(self, event_type=EventType.MESSAGE, **params):
         """ Send event using the provided parameters """
+        
+        # Only send, if event is in whitelist
         if event_type in self.whitelist:
             # Add server name to parameters for formatting
-            params["server_name"] = self.server_name
+            params["name"] = self.name
             message = safeformat(self.formats[event_type], **params)
             
             self._send_message(event_type, message)
     
     def _send_message(self, event_type, message):
+        """
+            Internal method to actually pass the message on.
+            To be overwritten by subclasses.
+        """
+        
+        print(message)
+
+class QueuedNotificationHandler(NotificationHandler):
+    """
+        Notification handler that uses a thread and a queue to handle events asynchronously
+    """
+    
+    class NotificationThread(threading.Thread):
+        def __init__(self, callback, name="keyboard-input-thread"):
+            self.callback = callback
+            self.event_queue = Queue()
+            self.wakeup_event = threading.Event()
+            
+            super(QueuedNotificationHandler.NotificationThread, self).__init__(name=name)
+            self.daemon = True
+            self.start()
+        
+        def add_event(self, event_type, message):
+            """ Add an event to the internal queue """
+            self.event_queue.put((event_type, message))
+            self.wakeup_event.set()
+        
+        def run(self):
+            while True:
+                if not self.event_queue.empty():
+                    # If the queue is not empty, there are events to handle
+                    event = self.event_queue.get()
+                    self.callback(*event)
+                else:
+                    # If queue is empty, sleep for 10s or until the wakeup_event is set
+                    self.wakeup_event.wait(timeout=10)
+                    self.wakeup_event.clear()
+    
+    def __init__(self, name="Server", event_whitelist=set([e for e in EventType]), event_formats=DEFAULT_EVENT_FORMATS):
+        super().__init__(name, event_whitelist, event_formats)
+        
+        self.thread = QueuedNotificationHandler.NotificationThread(self._handle_message)
+    
+    def _send_message(self, event_type, message):
+        self.thread.add_event(event_type, message)
+    
+    def _handle_message(self, event_type, message):
+        """
+            Method for handling events asynchronously.
+            To be overritten by subclasses.
+        """
+        
+        time.sleep(3)
         print(message)
