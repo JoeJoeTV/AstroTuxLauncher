579f05ba80a3a213c98c6c299de9e97848aa1dba
Johannes Schöneberger <hannes1033@gmail.com>
2024-03-31 23:29:30 +0200

    Add general exception classes

--- astrotux/utils/misc.py
+++ astrotux/utils/misc.py
@@ -17,3 +17,20 @@ def ensure_python_version():
         print(f"         You are currently running version {sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}.")
         print()
         sys.exit(1)
+
+
+# Exception classes to be used in the project
+class GeneralError(Exception):
+    """ General error, which should be logged, but doesn't necessarily cause the program to exit """
+    
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+class FatalError(GeneralError):
+    """ Fatal error, which should be logged and cause the program to exit """
+    
+    def __init__(self, message, exit_reason):
+        self.message = message
+        self.exit_reason = exit_reason
+        super().__init__(self.message)
